b03f199b38d81792f0bc160ade40762922593b49
Caroline-Martins <martins.tabares@gmail.com>
2021-08-20 00:17:42 -0300

    Resolução exercício 01 - Aula 05 - Módulo 01

---
+++ módulo 01/aula 05 - Resolução do exercício/exercicio-01.js
@@ -0,0 +1,7 @@
+nomeDoFulano = "Matheus"
+nomeDoBeltrano = "Luiz"
+
+mesmoNome = (nomeDoFulano == nomeDoBeltrano)
+console.log("Primeiro nome: " + nomeDoFulano)
+console.log("Segundo nome: " + nomeDoBeltrano)
+console.log("Primeiro nome é igual ao segundo nome? " + (mesmoNome?"sim":"não"))
