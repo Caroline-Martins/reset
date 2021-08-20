92aad1cd57809b33fc36503ce2e28559a08db2e7
Caroline-Martins <martins.tabares@gmail.com>
2021-08-20 19:48:39 -0300

    Resolução Exercício 02 aula- 05

--- módulo 01/aula 05 - Resolução do exercício/exercicio-01.js
+++ módulo 01/aula 05 - Resolução do exercício/exercicio-01.js
@@ -2,6 +2,7 @@ nomeDoFulano = "Caroline"
 nomeDoBeltrano = "caroline"
 
 mesmoNome = (nomeDoFulano.toUpperCase() == nomeDoBeltrano.toUpperCase())
+console.log("-Exercício 01")
 console.log("Primeiro nome: " + nomeDoFulano)
 console.log("Segundo nome: " + nomeDoBeltrano)
 console.log("Primeiro nome é igual ao segundo nome? " + (mesmoNome?"sim":"não"))
---
+++ módulo 01/aula 05 - Resolução do exercício/exercicio-02.js
@@ -0,0 +1,6 @@
+referenciaMaiorIdade=18
+idade= 24
+maiorDeIdade = (idade>=referenciaMaiorIdade)
+console.log("-Exercício 02")
+console.log("Idade: " + idade)
+console.log("É um adulto? " + (maiorDeIdade?" sim":"não")) 
