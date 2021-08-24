bbef33c78acbe3991c6d9bb9a40dde448706700b
Caroline-Martins <martins.tabares@gmail.com>
2021-08-23 22:37:08 -0300

    Exercício 03

---
+++ módulo 01/aula 05 - Resolução do exercício/exercicio-03.js
@@ -0,0 +1,8 @@
+// 3) Crie uma variável valorComJuros que calcula o valor de um boleto acrescido de 10% de juros por atraso;
+valorBoleto = 100
+valorComJuros = valorBoleto+(valorBoleto*0.1)
+console.log("O valor do boleto com juros é: " + valorComJuros)
+
+
+
+
